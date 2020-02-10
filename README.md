f6d8421e0bc3ad0a19723b28efe59ccb9febe944
zhanghuimeng <zhanghuimeng1997@gmail.com>
2020-02-10 15:10:42 +0800

    Added basic support

---
+++ README.md
@@ -0,0 +1,16 @@
+# arXiv Page Crawler
+
+Crawls a set of information from an [arXiv.org](arxiv.org) article.
+
+Installation:
+
+```cmd
+pip3 install beautifulsoup4
+pip3 install request
+```
+
+Usage:
+
+```cmd
+python crawler.py --link [link] --file [file of links]
+```
---
+++ crawler.py
@@ -0,0 +1,81 @@
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+template = u"""
+# %s
+
+链接：[%s](%s)
+
+作者：%s
+
+单位：
+
+出版：%s
+
+主题：%s
+
+要点：
+
+英文摘要：%s
+
+摘要：%s
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--link", type=str, help="A link")
+parser.add_argument("--file", type=str, help="A file of links", default="links.dat")
+parser.add_argument("--output", type=str, help="Output of crawlers", default="output.md")
+args = parser.parse_args()
+
+links = []
+if args.link:
+    links.append(args.link)
+if args.file:
+    with open(args.file) as f:
+        for line in f:
+            links.append(line.strip())
+
+with open(args.output, "w", encoding="utf-8") as f:
+    for link in links:
+        html = urlopen(link).read().decode("utf-8")
+        soup = BeautifulSoup(html, features="lxml")
+
+        title = soup.find("h1", {"class": "title mathjax"}).get_text()
+        title = title.replace("Title:", "").strip()
+        print("Title: %s" % title)
+
+        authors = soup.find("div", {"class": "authors"}).get_text()
+        authors = authors.replace("Authors:", "").strip()
+        print("Authors: %s" % authors)
+
+        comments = soup.find("td", {"class": "tablecell comments mathjax"})
+        if not comments:
+            comments = ""
+        else:
+            comments = comments.get_text()  
+        print("Comments: %s" % comments)
+
+        subjects = soup.find("td", {"class": "tablecell subjects"}).get_text()
+        subjects = subjects.strip()
+        print("Subjects: %s" % subjects)
+
+        abstract = soup.find("blockquote", {"class": "abstract mathjax"}).get_text()
+        abstract = abstract.replace("Abstract:", "").strip()
+        abstract = abstract.replace("\n", " ")
+        # print("Abstract: %s" % abstract)
+
+        ch_abstract = ""
+
+        pdf_link = soup.find("div", {"class": "full-text"}).find_all("ul")[0].find_all("li")[0].find_all("a")[0]["href"]
+        pdf_link = "https://arxiv.org" + pdf_link
+        print("Link: %s" % pdf_link)
+        # print("Downloading PDF...")
+        # r = requests.get(pdf_link, stream=True)    # stream loading
+        # with open("./data/%s.pdf" % title, "wb") as f2:
+        #     for chunk in r.iter_content(chunk_size=32):
+        #         f2.write(chunk)
+        
+        f.write(template % (title, link, link, authors, comments, subjects, abstract, ch_abstract))
+        print()
